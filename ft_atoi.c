e024f4f6f811effddf7c28b461416867432cea1b
ESPINOSA Tom <espinosa.tom7@gmail.com>
2025-10-30 15:42:44 +0100

    patching C files

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,11 @@
 /*   By: toespino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 15:11:21 by toespino          #+#    #+#             */
-/*   Updated: 2025/10/21 16:04:26 by toespino         ###   ########.fr       */
+/*   Updated: 2025/10/30 14:22:10 by toespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_atoi(const char *nptr)
 {
@@ -21,13 +23,14 @@ int	ft_atoi(const char *nptr)
 	sign = 1;
 	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
+	if (nptr[i] == '-')
 	{
-		if (nptr[i] == '-')
-			sign *= -1;
+		sign *= -1;
 		i++;
 	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	else if (nptr[i] == '+')
+		i++;
+	while (ft_isdigit(nptr[i]))
 	{
 		res = res * 10 + nptr[i] - 48;
 		i++;
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: toespino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 12:58:25 by toespino          #+#    #+#             */
-/*   Updated: 2025/10/29 14:15:18 by toespino         ###   ########.fr       */
+/*   Updated: 2025/10/30 14:17:54 by toespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	*ft_strjoin(const char *s1, const char *s2)
 
 	i = 0;
 	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
 	out = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
 	if (!out)
 		return (NULL);
